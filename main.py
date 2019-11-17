0fdc9791c3b981d727bd69bcc841736c91c3dfda
MilkshakeForReal <1577478107@qq.com>
2019-11-16 23:36:19 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set(style="darkgrid")
 
-from .utils import *
-from .model import Lars
+from utils import *
+from model import Lars
 
 def main():
     df = preprocess()

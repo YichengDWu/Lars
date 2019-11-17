9eae0e44390012f2dffad8d988b831b2a8e766d8
MilkshakeForReal <1577478107@qq.com>
2019-11-17 15:45:47 -0500

    Update model.py

--- model.py
+++ model.py
@@ -72,9 +72,8 @@ class Lars:
                 else:
                     gamma_tilde = np.inf
                 if gamma_tilde < gamma:
-                    print(f"The {j}th variable was dropped!")
                     print(A)
-                    A[j] = False
+                    A[A][j] = False
                     s = np.array([1 if c_>0 else -1 for c_ in c[A]])
                     #2.4
                     X_A =  s*X[:,A]

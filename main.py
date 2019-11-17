d18c5de8aed9d5d4969258cc6d6dec773c7c57e5
MilkshakeForReal <1577478107@qq.com>
2019-11-17 15:10:29 -0500

    Update main.py

--- main.py
+++ main.py
@@ -8,12 +8,18 @@ from utils import preprocess
 from model import Lars
 
 def main():
+    import argparse
+    parser = argparse.ArgumentParser('LAR')
+    parser.add_argument('-t', '--restrain', type=float, default=10000,
+                        help='Maximum 1 norm of coefs')
+    args = parser.parse_args().__dict__
+    
     df = preprocess()
     feats = ['cylinders', 'displacement', 'horsepower', 'weight',
                        'acceleration', 'model year']
     X = df[feats].values
     y = df[['mpg']].values
-    lar =Lars(feats)
+    lar =Lars(feats,args['restrain'])
     lar.fit(X, y.reshape(-1,1))
     print("R^2:", lar.score(X,y))
     

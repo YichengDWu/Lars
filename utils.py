ac98a0f1f8fa6432d7261c28bee6e7faf6419040
MilkshakeForReal <1577478107@qq.com>
2019-11-16 22:07:58 -0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -4,7 +4,7 @@ import numpy as np
 def preprocess():
     print("Starting processing data...")
     col_names = ['mpg','cylinders','displacement','horsepower','weight','acceleration','model year','origin','car name']
-    df = pd.read_table("../input/auto-mpg.data", header=None, delim_whitespace=True)
+    df = pd.read_table("./auto-mpg.data", header=None, delim_whitespace=True)
     df.columns = col_names
     df = df.drop('origin',axis = 1)
     df = df[df['horsepower'] != '?']

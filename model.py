b6955ce262e83f14da8ca96270bfff1e6ff031fc
MilkshakeForReal <1577478107@qq.com>
2019-11-17 10:57:03 -0500

    Update model.py

--- model.py
+++ model.py
@@ -73,17 +73,17 @@ class Lars:
         return pred+self.y_mean
     
     def plot_bar(self, i, c, beta):
-        fig, axs = plt.subplots(1,2, figsize = (20,8))
+        fig, axs = plt.subplots(1,2, figsize = (12,16))
         f1 = sns.barplot(x=self.feats,
                         y=beta.flatten(),palette='rocket',
                     ax = axs[0])
-        f1.set_xticklabels(axs[0].get_xticklabels(),rotation=45, fontsize = 15)
+        f1.set_xticklabels(axs[0].get_xticklabels(),rotation=45, fontsize = 8)
         plt.ylabel('Beta')      
         
         f2 = sns.barplot(x=self.feats,
                          y=c.flatten(),palette='rocket',
                          ax = axs[1])
-        f2.set_xticklabels(axs[1].get_xticklabels(),rotation=60,fontsize = 15)
+        f2.set_xticklabels(axs[1].get_xticklabels(),rotation=60,fontsize = 8)
         plt.ylabel('Correlation') 
         fig.suptitle(f'Step = {i}', fontsize=20)
         plt.show(block = False)

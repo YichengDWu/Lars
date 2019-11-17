5da60dab24e9eebb7764ba4ce71525cf5577e6f0
MilkshakeForReal <1577478107@qq.com>
2019-11-17 15:13:43 -0500

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,9 @@ def main():
     parser = argparse.ArgumentParser('LAR')
     parser.add_argument('-t', '--restrain', type=float, default=10000,
                         help='Maximum 1 norm of coefs')
+    parser.add_argument('-p', '--lasso_path', action='store_true',
+                        help='Coefficient Trajectories')
+    
     args = parser.parse_args().__dict__
     
     df = preprocess()
@@ -22,6 +25,8 @@ def main():
     lar =Lars(feats,args['restrain'])
     lar.fit(X, y.reshape(-1,1))
     print("R^2:", lar.score(X,y))
+    if args['lasso_path']:
+        lar.plot_path()
     
 if __name__ == '__main__':
     main()

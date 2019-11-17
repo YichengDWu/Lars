29644f3ff858d30f0b5b1bd068a6fe5446308674
MilkshakeForReal <1577478107@qq.com>
2019-11-16 23:50:53 -0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -4,15 +4,16 @@ import numpy as np
 def preprocess():
     print("Starting processing data...")
     col_names = ['mpg','cylinders','displacement','horsepower','weight','acceleration','model year','origin','car name']
-    df = pd.read_table("./auto-mpg.data", header=None, delim_whitespace=True)
+    df = pd.read_table("../input/auto-mpg.data", header=None, delim_whitespace=True)
     df.columns = col_names
-    df = df.drop('origin',axis = 1)
+    df = df.drop(['origin','car name'],axis = 1)
     df = df[df['horsepower'] != '?']
     df['horsepower'] = df['horsepower'].astype('float')
     print("Done!")
     return df
 
 def normalize(X):
-    norms = np.linalg.norm(X, axis = 0)
     means = np.mean(X, axis = 0)
-    return (X-means)/norms
+    X = X - means
+    norms = np.linalg.norm(X, axis = 0)
+    return X/norms

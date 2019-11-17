dc02380f94c04d6b8ee9543954aefcd02b5b0b3c
MilkshakeForReal <1577478107@qq.com>
2019-11-16 23:32:35 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set(style="darkgrid")
+
+from .utils import *
+from .model import Lars
+
+def main():
+    df = preprocess()
+    X = df[['cylinders', 'displacement', 'horsepower', 'weight',
+            'acceleration', 'model year']].values
+    y = df[['mpg']].values
+    lar =Lars()
+    lar.fit(X, y.reshape(-1,1))
+    
+if __name__ == '__main__':
+    main()

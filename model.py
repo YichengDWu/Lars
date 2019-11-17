bd84fb9877ce7495f5b221b1e8bb050939513f42
MilkshakeForReal <1577478107@qq.com>
2019-11-17 16:42:26 -0500

    Update model.py

--- model.py
+++ model.py
@@ -135,6 +135,7 @@ class Lars:
         plt.show(block = False)
         
     def score(self, X, y_true):
+        X = normalize(X)
         y_pred = self.predict(X)
         sse = ((y_true - y_pred) ** 2).sum(axis=0, dtype=np.float64)
         tse = ((y_true - np.average(y_true, axis=0)) ** 2).sum(axis=0, dtype=np.float64)
